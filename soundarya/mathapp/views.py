ce2c8d9dea67b5ea7b4ea161cbfe70791e0ff4da
soundaryajothi <soundaryajothi213@gmail.com>
2023-11-25 08:56:36 +0530

    Success

---
+++ soundarya/mathapp/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+def prismarea(request):
+    context={}
+    context['area'] = "0"
+    context['a'] = "0"
+    context['h'] = "0"
+    if request.method == 'POST':
+        print("POST method is used")
+        a = request.POST.get('base edge','0')
+        h = request.POST.get('height','0')
+        print('request=',request)
+        print('Base edge=',a)
+        print('Height=',h)
+        area = 2*(int(a)*int(a)) + 4*int(a)*int(h)
+        context['area'] = area
+        context['a'] = a
+        context['h'] = h
+        print('Area=',area)
+    return render(request,'mathapp/math.html',context)
+
